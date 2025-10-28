0bdb06bae9f1043aff95b4032b04eeb62dd02bcb
gauravputale1416 <gauravputale2003@gmail.com>
2025-10-29 00:44:43 +0530

    add some port

--- server/index.js
+++ server/index.js
@@ -8,14 +8,7 @@ const app=express();
     app.use(express.json());
     app.use(cors());
 
-    const connectDB= async ()=>{
-        const conn=await mongoose.connect(process.env.MONGODB_URL);
-        if(conn){
-            console.log("mongo-db conected");
-            console.log("MONGO_URI:", process.env.MONGO_URI);
-
-        }
-    }
+  
 app.get("/",(res,req)=>{
     res.json({
         message:"server is on ",
@@ -25,5 +18,5 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
     console.log(`sever is on ${PORT}`);
-    connectDB();
+    
 })
