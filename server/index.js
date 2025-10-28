b466a988e61165d07d1912443daae416d4d7e262
gauravputale1416 <gauravputale2003@gmail.com>
2025-10-28 23:42:29 +0530

    added server code

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import express from "express"
+dotenv.config()
+
+const app=express();
+    app.use(express.json());
+    app.use(cors());
+
+    const connectDB= async ()=>{
+        const conn=await mongoose.connect(process.env.MONGODB_URL);
+        if(conn){
+            console.log("mongo-db conected");
+        }
+    }
+app.get("/",(res,req)=>{
+    res.json({
+        message:"server is on ",
+    });
+});
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`sever is on ${PORT}`);
+    connectDB();
+})
